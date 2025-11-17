9afddf7ecb5d59d3974f14c7ad52419de566384f
sjmcilwain <sjmcilwain@wisc.edu>
2025-11-17 15:27:05 -0600

    updating deploy.R

---
+++ deploy.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+#
+#  pliu 20211031
+#  smcilwain 20221104
+#  smcilwain 20251107
+#
+#  deploy Shiny App to data-viz.it.wisc.edu
+#  - if deploy the first time, need to connect via RStudio:
+#    - runApp -> publish -> add account -> add server -> connect
+#  - please keep rsconnect/ folder so that updated app will be deployed under
+#    the same appID and url as well as saving deployment time
+#
+#  Results:
+#  - https://data-viz.it.wisc.edu/content/223/
+#
+
+#devtools::install_github("cisrlab/itcpredictr")
+
+rsconnect::deployApp( appDir  = './',
+                      appName = 'itc_predict',
+                      account = 'sjmcilwain', ## change to your wisc NetID
+                      server  = 'connect.doit.wisc.edu' #,
+#		      forceUpdate = TRUE
+)
