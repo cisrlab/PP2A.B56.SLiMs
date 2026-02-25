d98c4290bdc89df231c1091911a5a9b1cbff8363
sjmcilwain <sjmcilwain@wisc.edu>
2026-02-25 12:28:58 -0600

    Updating app

--- deploy.R
+++ deploy.R
@@ -15,7 +15,7 @@
 #  - https://data-viz.it.wisc.edu/content/223/
 #
 
-devtools::install_github("cisrlab/itcpredictr@fimo")
+devtools::install_github("cisrlab/PP2A.B56.SLiMs@fimo")
 
 rsconnect::deployApp( appDir  = './',
                       appName = 'itc_predict',
